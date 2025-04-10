5fd1a33ae74af8abd7963e8f27ae3ce08a0d5d26
Prinstonlee <33902487+Prinstonlee@users.noreply.github.com>
2025-04-10 22:25:33 +0800

    chris lastest

--- Login.java
+++ Login.java
@@ -1,15 +1,15 @@
 public class Login {
-    // Private fields (encapsulated)
+    // Private data fields (encapsulation)
     private String username;
     private String password;
     private boolean isAdmin;
-    // Constructor & methods to control access
+    // Constructors
     public Login(String username, String password, boolean isAdmin) {
         this.username = username;
         this.password = password;
         this.isAdmin = isAdmin;
     }
-
+    //set and get methods
     public boolean authenticate(String username, String password) {
         return this.username.equals(username) && this.password.equals(password);
     }
